dcc4f3f35ddde953f238252c5e1cfb3ab56c7d44
Eadweard <aurora.coder.zqf@gmail.com>
2025-03-05 22:49:05 +0800

    fix all

--- src/feature/handler.ts
+++ src/feature/handler.ts
@@ -1,9 +1,11 @@
 import { i18nAllStr } from './config'
+import { regexMatchI18nStr } from './help'
 
 /**
  * 获取当前浏览器url的国际化部分
+ * @param defaultI18n 当前默认国际化语言
  */
-export function getCurrentUrlI18n(defaultI18n = 'zh') {
+export function getCurrentUrlI18n(defaultI18n = ''): string | undefined {
   if (typeof window !== 'undefined') {
     const url = window.location.href
     const urlObj = new URL(url)
@@ -11,8 +13,8 @@ export function getCurrentUrlI18n(defaultI18n = 'zh') {
 
     const firstPathChunks = pathname.split('/')
     const firstPathChunkStr = firstPathChunks[1]
-    if (firstPathChunkStr) {
-
+    if (firstPathChunkStr && regexMatchI18nStr(firstPathChunkStr)) {
+      return firstPathChunkStr
     }
     else {
       return defaultI18n
@@ -20,5 +22,17 @@ export function getCurrentUrlI18n(defaultI18n = 'zh') {
   }
   else {
     throw new TypeError('getCurrentUrlI18n只能在浏览器环境下使用')
+  }
+}
+
+/**
+ * 根据当前语言组装新的url
+ */
+export function assembleNewUrl(language: string, url: string) {
+  if (language) {
+    return `/${language}${url}`
+  }
+  else {
+    return url
   }
 }
